90c39c9def44708743ce605d6a1a40ce39af39ba
Kenley Arai <me@kenleyarai.com>
2016-10-27 09:15:18 -0700

    Fixed bug because of undefinded key

--- app/views/index.py
+++ app/views/index.py
@@ -3,4 +3,6 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("index.html", result=session['credentials']['id_token']['email'] if session['credentials']['id_token']['email'] else None)
+    if 'credentials' in session:
+        return render_template("index.html", result=session['credentials']['id_token']['email'] if session['credentials']['id_token']['email'] else None)
+    return render_template("index.html"
