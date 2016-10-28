9994becf0d6fb73444b9f8265baceeacdd24ce60
James Hollister <jhollisterjr@gmail.com>
2016-10-28 15:03:02 -0700

    Removed https function

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,4 @@ app.secret_key = os.environ["SECRET"]
 with open("client.txt", "w") as text_file:
     print(os.environ["GOOGLE"], file=text_file)
 
-@app.before_request
-def before_request():
-    if request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
-
 from views import index, login, logout
