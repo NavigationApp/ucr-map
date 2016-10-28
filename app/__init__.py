8959b68d98db754054a7601539f0dfc8182de912
Kenley Arai <me@kenleyarai.com>
2016-10-27 18:41:44 -0700

    Added file stuff

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET"]
 
+with open("client.txt", "w") as text_file:
+    print(os.environ["GOOGLE"], file=text_file)
+
 from views import index, login
--- app/views/login.py
+++ app/views/login.py
@@ -13,8 +13,9 @@ def login(provider_name):
     """
     output = StringIO.StringIO()
     output.write(os.environ["GOOGLE"])
+
     flow = client.flow_from_clientsecrets(
-        output,
+        "client.txt",
         scope=['email','profile'],
         redirect_uri = url_for('login', provider_name="google", _external=True))
     output.close()
