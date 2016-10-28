57dbedd3f7f39db9c35f612a3792ee1ff8103c6d
Kenley Arai <me@kenleyarai.com>
2016-10-27 18:36:34 -0700

    Using stringio instead of FileSpoof

--- app/views/login.py
+++ app/views/login.py
@@ -2,27 +2,22 @@ from flask import request, url_for, redirect,session
 from app import app
 import os
 from oauth2client import client
-
+import StringIO
 import json
 
-
-class FileSpoof(object):
-    def __init__(self, my_text):
-        self.my_text = my_text
-
-    def readlines(self):
-        return self.my_text.splitlines()
 
 @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
 def login(provider_name):
     """
     Login handler, must accept both GET and POST to be able to use OpenID.
     """
-
+    output = StringIO.StringIO()
+    output.write(os.environ["GOOGLE"])
     flow = client.flow_from_clientsecrets(
-        FileSpoof(os.environ["GOOGLE"]),
+        FileSpoof(),
         scope=['email','profile'],
         redirect_uri = url_for('login', provider_name="google", _external=True))
+    output.close()
     if 'code' not in request.args:
         auth_uri = flow.step1_get_authorize_url()
         return redirect(auth_uri)
