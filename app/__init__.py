012baaf5624d2b61a5471248619e70dfaa760946
Kenley Arai <me@kenleyarai.com>
2016-10-28 15:01:13 -0700

    Forcing HTTPS

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
 
 import os
-from flask import Flask
+from flask import Flask,request,redirect
 
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET"]
 
 with open("client.txt", "w") as text_file:
     print(os.environ["GOOGLE"], file=text_file)
-    
+
 @app.before_request
 def before_request():
     if request.url.startswith('http://'):
