57e2bdf46e811544c5d896dd528f6aaf7c714ede
Kenley Arai <me@kenleyarai.com>
2016-11-01 14:17:44 -0700

    Enforcing https

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
 
 import os
-from flask import Flask,request,redirect
+from flask import Flask
+from flask_sslify import SSLify
 
 app = Flask(__name__)
+sslify = SSLify(app)
 app.secret_key = os.environ["SECRET"]
+app.debug = False
 
 with open("client.txt", "w") as text_file:
     print(os.environ["GOOGLE"], file=text_file)
