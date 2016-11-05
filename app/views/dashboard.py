b217149935f0716c6114349a7858c37fd1d7269b
Kenley Arai <me@kenleyarai.com>
2016-11-05 12:14:48 -0700

    made the passing of user more uniform

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,9 +1,10 @@
 from flask import render_template
 from flask_security.decorators import roles_required
+from flask_security.core import current_user
 from app import app
 
 
 @app.route('/dashboard')
 @roles_required('Admin')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", user=current_user)
--- app/views/index.py
+++ app/views/index.py
@@ -6,5 +6,5 @@ from app import app
 @app.route('/')
 def index():
     if current_user.is_authenticated:
-        return render_template("index.html", result=current_user.connections.full_name)
+        return render_template("index.html", user=current_user)
     return render_template("index.html")
