02e96637b4476fce15c0bcb6a7024d606074fa89
Kenley Arai <me@kenleyarai.com>
2016-11-05 12:20:00 -0700

    added basic display of users

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from flask_security.decorators import roles_required
 from flask_security.core import current_user
-from app import app, User
+from app import app
+from app.models import User
 
 
 @app.route('/dashboard')
