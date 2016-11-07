a6a0ea0ab5413b042a54fc8f3a4aea267a285c5b
Kenley Arai <me@kenleyarai.com>
2016-11-07 14:04:34 -0800

    added updating roles

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -3,7 +3,6 @@ from flask_security.decorators import roles_required
 from flask_security.core import current_user
 from app import app
 from app.models import User
-from app import db, user_datastore
 
 
 @app.route('/dashboard')
@@ -16,9 +15,9 @@ def dashboard():
 @app.route('/dashboard/<id>/<role>')
 @roles_required('Admin')
 def dashboard_edit(id, role):
-    role = user_datastore.find_or_create_role(role)
-    user = user_datastore.get_user(id)
-    user_datastore.add_role_to_user(user, role)
+    role = app.user_datastore.find_or_create_role(role)
+    user = app.user_datastore.get_user(id)
+    app.user_datastore.add_role_to_user(user, role)
     return render_template("dashboard.html")
 
 
@@ -26,5 +25,5 @@ def dashboard_edit(id, role):
 @roles_required('Admin')
 def dashboard_delete(id):
     user = User.query.filter_by(id=id).first()
-    user_datastore.delete_user(user)
+    app.user_datastore.delete_user(user)
     return render_template("dashboard.html")
