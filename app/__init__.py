516b02eeed39318d8da150842a9361537a2fa646
Kenley Arai <me@kenleyarai.com>
2016-11-07 14:13:53 -0800

    added updating roles

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_sslify import SSLify
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 from flask.ext.social.datastore import SQLAlchemyConnectionDatastore
-from flask_security import Security, SQLAlchemyUserDatastore,login_user
+from flask_security import Security, SQLAlchemyUserDatastore,login_user, roles_required
 from flask_social.views import connect_handler
 from flask.ext.social.utils import get_connection_values_from_oauth_response
 from pprint import pprint
@@ -37,10 +37,26 @@ from views import index, logout, dashboard
 
 # Setup Flask-Security
 from app.models import User, Role, Connection
-app.user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 app.security = Security(app, user_datastore)
 app.social = Social(app, SQLAlchemyConnectionDatastore(db, Connection))
 heroku.init_app(app)
+
+@app.route('/dashboard/<id>/<role>')
+@roles_required('Admin')
+def dashboard_edit(id, role):
+    role = user_datastore.find_or_create_role(role)
+    user = user_datastore.get_user(id)
+    user_datastore.add_role_to_user(user, role)
+    return render_template("dashboard.html")
+
+
+@app.route('/dashboard/delete/<id>')
+@roles_required('Admin')
+def dashboard_delete(id):
+    user = User.query.filter_by(id=id).first()
+    user_datastore.delete_user(user)
+    return render_template("dashboard.html")
 
 # Setting up new users
 @login_failed.connect_via(app)
--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,28 +1,12 @@
 from flask import render_template, request
 from flask_security.decorators import roles_required
 from flask_security.core import current_user
-from app import app, user_datastore
+from app import app
 from app.models import User
+
 
 @app.route('/dashboard')
 @roles_required('Admin')
 def dashboard():
     users = User.query.all()
     return render_template("dashboard.html", user=current_user, users=users)
-
-
-@app.route('/dashboard/<id>/<role>')
-@roles_required('Admin')
-def dashboard_edit(id, role):
-    role = user_datastore.find_or_create_role(role)
-    user = user_datastore.get_user(id)
-    user_datastore.add_role_to_user(user, role)
-    return render_template("dashboard.html")
-
-
-@app.route('/dashboard/delete/<id>')
-@roles_required('Admin')
-def dashboard_delete(id):
-    user = User.query.filter_by(id=id).first()
-    user_datastore.delete_user(user)
-    return render_template("dashboard.html")
