5627855372f432bd163faf986f42a9093c8c531a
Kenley Arai <me@kenleyarai.com>
2016-11-07 18:58:46 -0800

    Fixed typo

--- app/models.py
+++ app/models.py
@@ -51,7 +51,7 @@ class User(db.Model):
             return self
 
     def is_friend(self, user):
-        return self.friended.filter(friends.c.followed_id == user.id).count() > 0
+        return self.friended.filter(friends.c.friended_id == user.id).count() > 0
 
     def has_role(self, role_check):
         return role_check in self.roles
