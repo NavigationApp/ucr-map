fc33b96af15990f415eefcd4ce25bb892ec2e821
Antoine Guerrero <asguerrero3@gmail.com>
2016-11-17 15:12:30 -0800

    Move stylesheets ans scripts into blocks

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,65 +1,63 @@
-// base.html
 /*
- todo: we are currently limited to http only connections for sockets due to the http URL. If the
- URL is changed to https connections are still broke. We need to transition to a more robust way
- to communicate between the server and client side.
- */
-var socket = io.connect('http://' + document.domain + ':' + location.port);
+     todo: we are currently limited to http only connections for sockets due to the http URL. If the
+     URL is changed to https connections are still broke. We need to transition to a more robust way
+     to communicate between the server and client side.
+     */
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-socket.on('connect', function (data) {
-    socket.emit("get_all_friends")
-});
-
-// Handle search for new friends
-var inputBox = document.getElementById('sfriends');
-socket.on('names', function (data) {
-    Object.keys(data).forEach(function (key) {
-        var users = document.getElementById('search-list');
-        users.innerHTML = "";
-        for (var key in data) {
-            if (data.hasOwnProperty(key)) {
-                var val = data[key];
-                var entry = document.createElement('li');
-                var button = document.createElement('button');
-                button.id = "myButton" + key;
-                button.className = "btn btn-primary pull-right";
-                button.autocomplete = "off";
-                button.setAttribute('onclick', "socket.emit('add', {add: " + key + "})");
-                button.innerHTML = '<i class="fa fa-user-plus" aria-hidden="true"></i>';
-                entry.className = "list-group-item";
-                entry.id = key;
-                entry.appendChild(document.createTextNode(val));
-                entry.appendChild(button)
-                users.appendChild(entry);
-
-            }
-        }
+    socket.on('connect', function (data) {
+        socket.emit("get_all_friends")
     });
-});
-inputBox.onkeyup = function () {
-    socket.emit('search', {search: inputBox.value});
-}
 
-// Handle display current friends
-var friend_list_box = document.getElementById('current_friends');
-socket.on('my_friends', function (data) {
-    Object.keys(data).forEach(function (key) {
-        var users = document.getElementById('friends-list');
-        users.innerHTML = "";
-        for (var key in data) {
-            if (data.hasOwnProperty(key)) {
-                var val = data[key];
-                var entry = document.createElement('li');
-                entry.className = "list-group-item";
-                entry.id = key;
-                entry.appendChild(document.createTextNode(val));
-                users.appendChild(entry);
+    // Handle search for new friends
+    var inputBox = document.getElementById('sfriends');
+    socket.on('names', function (data) {
+        Object.keys(data).forEach(function (key) {
+            var users = document.getElementById('search-list');
+            users.innerHTML = "";
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    var val = data[key];
+                    var entry = document.createElement('li');
+                    var button = document.createElement('button');
+                    button.id = "myButton" + key;
+                    button.className = "btn btn-primary pull-right";
+                    button.autocomplete = "off";
+                    button.setAttribute('onclick', "socket.emit('add', {add: " + key + "})");
+                    button.innerHTML = '<i class="fa fa-user-plus" aria-hidden="true"></i>';
+                    entry.className = "list-group-item";
+                    entry.id = key;
+                    entry.appendChild(document.createTextNode(val));
+                    entry.appendChild(button)
+                    users.appendChild(entry);
 
+                }
             }
-        }
+        });
     });
-});
-friend_list_box.onkeyup = function () {
-    socket.send('get_friend');
-}
+    inputBox.onkeyup = function () {
+        socket.emit('search', {search: inputBox.value});
+    }
 
+    // Handle display current friends
+    var friend_list_box = document.getElementById('current_friends');
+    socket.on('my_friends', function (data) {
+        Object.keys(data).forEach(function (key) {
+            var users = document.getElementById('friends-list');
+            users.innerHTML = "";
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    var val = data[key];
+                    var entry = document.createElement('li');
+                    entry.className = "list-group-item";
+                    entry.id = key;
+                    entry.appendChild(document.createTextNode(val));
+                    users.appendChild(entry);
+
+                }
+            }
+        });
+    });
+    friend_list_box.onkeyup = function () {
+        socket.send('get_friend');
+    }
